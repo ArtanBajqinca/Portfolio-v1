2dd1372b0af7ba8a78c842c0a213d4a95f8eb9e9
Arctan <artan.bajqinca@gmail.com>
2023-09-22 17:25:46 +0200

    Changed folder structure

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express"); // loads the package
-const { engine } = require("express-handlebars"); // loads handlebars
-const port = 1111; // defines the port
+const express = require("express"); // loads the express package
+const { engine } = require("express-handlebars"); // loads handlebars for Express
+const port = 8080; // defines the port
 const app = express(); // creates the Express application
 
 // defines handlebars engine
@@ -9,7 +9,8 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 // defines the views directory
 app.set("views", "./views");
-// define static directory "public"
+
+// define static directory "public" to access css/ and img/
 app.use(express.static("public"));
 
 // MODEL (DATA)
@@ -17,12 +18,24 @@ const humans = [
   { id: "0", name: "Jerome" },
   { id: "1", name: "Mira" },
   { id: "2", name: "Linus" },
+  { id: "3", name: "Susanne" },
+  { id: "4", name: "Jasmin" },
 ];
 
 // CONTROLLER (THE BOSS)
-app.get("/", (request, response) => {
-  const model = humans[0];
+app.get("/", function (request, response) {
+  response.render("home.handlebars");
+});
+
+app.get("/humans", (request, response) => {
+  const model = { listHumans: humans };
   response.render("humans.handlebars", model);
+});
+
+app.get("/humans/:id", (request, response) => {
+  const id = request.params.id;
+  const model = humans[id];
+  response.render("human.handlebars", model);
 });
 
 // runs the app and listens to the port
